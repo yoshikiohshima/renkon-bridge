df1f637f415307a4b0918c0f349794db21c9bd2e
Yoshiki Ohshima <yoshiki.ohshima@acm.org>
2024-11-12 15:36:45 -0800

    use audio processor to get samples

--- bridge/audio-visualizer.js
+++ bridge/audio-visualizer.js
@@ -7,24 +7,12 @@ class VisualizerProcessor extends AudioWorkletProcessor {
     }
 
     process(inputs, _outputs, _parameters) {
-        let now = Date.now();
-        if (now < this.lastUpdateTime + this.updateIntervalInMS) {return true;}
-        this.lastUpdateTime = now;
-        const input = inputs[0];
+        // let now = Date.now();
+        // if (now < this.lastUpdateTime + this.updateIntervalInMS) {return true;}
+        // this.lastUpdateTime = now;
+        //const input = inputs[0];
 
-        if (input.length > 0) {
-            const samples = input[0];
-            let max = 0;
-
-            for (let i = 0; i < samples.length; i++) {
-                max = Math.max(max, Math.abs(samples[i]));
-            }
-
-            max = Math.max((max * 10 - 0.5), 0); // hmm
-            this.volume = max;
-
-            this.port.postMessage({volume: this.volume});
-        }
+        this.port.postMessage({inputs});
         return true;
     }
 }
