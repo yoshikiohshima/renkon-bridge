1679dda6df3b118b64d10c10e777967c86028ac2
Yoshiki Ohshima <yoshiki.ohshima@acm.org>
2024-11-15 15:33:33 -0800

    try to use async and see what happens

--- bridge/audio-samples.js
+++ bridge/audio-samples.js
@@ -4,15 +4,18 @@ class VisualizerProcessor extends AudioWorkletProcessor {
         this.id = 0;
     }
 
-    process(inputs, _outputs, _parameters) {
+    process(inputs, outputs, _parameters) {
         const input = inputs[0];
         let max = 0;
-        if (!input[0]) {debugger; return false}
+        if (!input[0]) {
+            console.log("no input");
+            return true;
+        }
         for (let i = 0; i < input[0].length; i++) {
             max = Math.abs(input[0][i]) > max ? Math.abs(input[0][i]) : max;
         }
-        
-        this.port.postMessage({id: this.id, input});
+        outputs[0][0].set(input[0], 0);
+        this.port.postMessage({id: this.id, input, max});
         this.id++;
         return true;
     }
